7a9d58e1c70bc1c4fb6510514a61cc57a27177ba
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-02-28 19:48:02 +0100

    zprovozneni deploy pro CI

--- up/settings.py
+++ up/settings.py
@@ -144,10 +144,10 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'assets'),
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
