ed3306cf483364a65b58c5dc3d1357479b6816be
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-02-28 20:57:34 +0100

    allowed_hosts povoleni heroku

--- up/settings.py
+++ up/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u-y&tu264zgfp1pc$q(xki+y^+c6zm2#z++%$xqn=!tum8i6e!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['uspesnyprvnacek.herokuapp.com']
 
 
 # Application definition
