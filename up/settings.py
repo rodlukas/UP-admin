20eeefabae325debed4cfce34210b172e6450c4f
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-02-28 23:55:37 +0100

    oprava pro CI a heroku

--- up/settings.py
+++ up/settings.py
@@ -150,7 +150,4 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
