b8bbd6f49aa2a9077d8dfe3b869cd2266f609b83
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-02-28 21:59:45 +0100

    oprava nefunkcniho reactu pri buildu pro produkci

--- up/settings.py
+++ up/settings.py
@@ -147,5 +147,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
