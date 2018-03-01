9ebc2fa3e52e5466d38750bdf3e2b8f889bf0b2b
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-03-01 12:08:30 +0100

    oprava DB pro heroku

--- up/settings.py
+++ up/settings.py
@@ -99,16 +99,16 @@ WSGI_APPLICATION = 'up.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+#    'default': {
+ #       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+  #      'NAME': 'postgres',
+   #     'USER': 'postgres',
+   #     'PASSWORD': 'postgres',
+    #    'HOST': 'localhost',
+   #     'PORT': '5432',
+#    }
+#}
 
 
 # Password validation
