eeb1aca7a5e16a1277c40053e65e3d041ff5da5a
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-03-04 19:46:58 +0100

    zkusebni oprava server error na heroku pro API

--- up/production_settings.py
+++ up/production_settings.py
@@ -15,7 +15,7 @@ WEBPACK_LOADER = {
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = False
+# DEBUG = False
 
 # Heroku: Update database configuration from $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=60)
