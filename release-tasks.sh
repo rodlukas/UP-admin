1a920da2e6d0f37ccd4b28ab5a448beaa49aa24d
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-04-10 23:27:29 +0200

    vylepseni CI

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 export DJANGO_SETTINGS_MODULE=up.production_settings
 python manage.py collectstatic --noinput --settings=up.production_settings
 python manage.py migrate --settings=up.production_settings
