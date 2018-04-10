9805ae7248baa70fbc9d949e4114ab3d87be702e
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-04-10 14:34:22 +0200

    vetsi zabezpeceni - uprava produkcniho nastaveni

--- up/production_settings.py
+++ up/production_settings.py
@@ -13,6 +13,12 @@ WEBPACK_LOADER = {
 }
 
 DEBUG = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
 
 # Heroku: Update database configuration from $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=CONST_DB_CON_AGE)
