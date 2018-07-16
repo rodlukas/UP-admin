d4fdf20192a8bc26d5d0095828bece25acadaae1
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-07-16 22:53:03 +0200

    podrobnější informace při časovém konfliktu (jméno klienta/skupiny), fixes #30

--- api/serializers.py
+++ api/serializers.py
@@ -209,6 +209,11 @@ class LectureSerializer(serializers.ModelSerializer):
             # tvorba errormsg
             err_datetime = local_dt.strftime("%d. %m. %Y - %H:%M")
             err_duration = str(elem.duration)
-            err = 'Časový konflikt s jinou lekcí (' + err_datetime + ', trvání ' + err_duration + ' min.)'
+            err = 'Časový konflikt s jinou lekcí (' + err_datetime + ', trvání ' + err_duration + ' min.), '
+            if elem.group is not None:
+                err += 'skupina ' + elem.group.name
+            else:
+                client = elem.attendances.get().client
+                err += 'klient ' + client.surname + ' ' + client.name
             raise serializers.ValidationError({api_settings.NON_FIELD_ERRORS_KEY: [err]})
         return data
