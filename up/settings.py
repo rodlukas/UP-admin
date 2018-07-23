2fb039e546cb002926269842eeb73fdb64651197
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-07-23 14:29:14 +0200

    Sentry aktivní pouze na produkci (nikoliv na lokálu)

--- up/settings.py
+++ up/settings.py
@@ -27,54 +27,6 @@ INSTALLED_APPS = [
     'raven.contrib.django.raven_compat',
 ]
 
-RAVEN_CONFIG = {
-    'dsn': 'https://45ed918ced1d4453914265b6bb4f98ab:3f6efffbcd9746979ab8339d312badcb@sentry.io/1247206',
-}
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'root': {
-        'level': 'WARNING',
-        'handlers': ['sentry'],
-    },
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s '
-                      '%(process)d %(thread)d %(message)s'
-        },
-    },
-    'handlers': {
-        'sentry': {
-            'level': 'WARNING',  # To capture more than ERROR, change to WARNING, INFO, etc.
-            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
-            'tags': {'custom-tag': 'x'},
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'django.db.backends': {
-            'level': 'ERROR',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'raven': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'sentry.errors': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-    },
-}
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
