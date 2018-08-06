64b36634b3beac5b3d66b0835abf1a812487a1cd
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-08-06 21:05:56 +0200

    odkaz na verzi aplikace je bílý

--- frontend/src/components/AppVersion.js
+++ frontend/src/components/AppVersion.js
@@ -1,8 +1,9 @@
 import React, {Fragment} from "react"
+import "./AppVersion.css"
 
 const AppVersion = () =>
     <Fragment>
-        <a href="https://github.com/rodlukas/UP-admin/commit/GIT_VERSION">GIT_VERSION</a>
+        <a href="https://github.com/rodlukas/UP-admin/commit/GIT_VERSION" className="AppVersion">GIT_VERSION</a>
     </Fragment>
 
 export default AppVersion
