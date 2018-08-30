086cbf6f59f051dbb6bd58766f3d5b68f415ad3b
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-08-30 13:41:39 +0200

    oprava tvorby .npmrc souboru

--- scripts/create_npmrc.sh
+++ scripts/create_npmrc.sh
@@ -5,7 +5,11 @@ if [ -z "${FONTAWESOME_TOKEN}" ]; then
 else
     ls
     NPMRC_CONTENT="//npm.fontawesome.com/:_authToken=${FONTAWESOME_TOKEN}\n"
-    printf NPMRC_CONTENT >> ../.npmrc
-    echo "soubor /.nmprc byl uspesne vytvoren"
+    printf NPMRC_CONTENT >> /frontend/.npmrc
+    echo "soubor .nmprc byl uspesne upraven"
+    cd ../
     ls
+    cd frontend
+    ls
+    cd ..
 fi
