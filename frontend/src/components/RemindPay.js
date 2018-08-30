2baa0de9748bcd026831843d9d8beb7c7a796b97
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-08-30 16:15:14 +0200

    nová ikona RemindPay (info předtím připomínalo vykřičník z formulářů)

--- frontend/src/components/RemindPay.js
+++ frontend/src/components/RemindPay.js
@@ -1,11 +1,11 @@
 import React from "react"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faExclamationCircle} from "@fortawesome/pro-solid-svg-icons"
+import {faCommentDollar} from "@fortawesome/pro-solid-svg-icons"
 
 const RemindPay = ({remind_pay}) =>
     remind_pay &&
     <span title="Příště platit">
-        <FontAwesomeIcon icon={faExclamationCircle} size="lg" color="#ff9800" transform="up-4"/>
+        <FontAwesomeIcon icon={faCommentDollar} size="lg" color="#ff9800" transform="up-4"/>
     </span>
 
 export default RemindPay
