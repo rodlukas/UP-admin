bf224c1b595837e35f9f09c5a2ac7803d2de9cbe
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-09-10 10:23:25 +0200

    aktualizace deps pro python a js, diky nove verzi react-fontawesome zjednoduseni RemindPay title

--- frontend/src/components/RemindPay.js
+++ frontend/src/components/RemindPay.js
@@ -4,8 +4,6 @@ import {faCommentDollar} from "@fortawesome/pro-solid-svg-icons"
 
 const RemindPay = ({remind_pay}) =>
     remind_pay &&
-    <span title="Příště platit">
-        <FontAwesomeIcon icon={faCommentDollar} size="lg" color="#ff9800" transform="up-4"/>
-    </span>
+    <FontAwesomeIcon icon={faCommentDollar} size="lg" color="#ff9800" transform="up-4" title="Příště platit"/>
 
 export default RemindPay
