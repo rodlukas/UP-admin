bbf4d7ee786c34ff1fbcfbe6702f2ef1ce64f7ff
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-09-10 08:24:37 +0200

    indikace práce při delším načítání než 5 sekund

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,12 +1,30 @@
-import React from "react"
+import React, {Component} from "react"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faSpinnerThird} from "@fortawesome/pro-solid-svg-icons"
 
-const Loading = ({text = "Načítání"}) =>
-    <div className="text-center">
-        <FontAwesomeIcon icon={faSpinnerThird} spin size="3x"/>
-        <br/>
-        {text}...
-    </div>
+const LONG_LOADING_THRESHOLD = 5 // sekundy
 
-export default Loading
+export default class Loading extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            LONG_LOADING: false
+        }
+        this.timeoutId = setTimeout(() => this.setState({LONG_LOADING: true}), LONG_LOADING_THRESHOLD * 1000)
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timeoutId)
+    }
+
+    render() {
+        return (
+            <div className="text-center">
+                <FontAwesomeIcon icon={faSpinnerThird} spin size="3x"/>
+                <br/>
+                {this.props.text ? this.props.text : "Načítání"}...
+                {this.state.LONG_LOADING && " Stále pracuji 😏"}
+            </div>
+        )
+    }
+}
