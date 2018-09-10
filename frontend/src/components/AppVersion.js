c8ffd679fb42dadbff08c9e733e0559e1d4b18da
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-09-10 08:50:35 +0200

    datum a čas commitu v title odkazu v liště s menu

--- frontend/src/components/AppVersion.js
+++ frontend/src/components/AppVersion.js
@@ -3,7 +3,7 @@ import "./AppVersion.css"
 
 const AppVersion = () =>
     <a href="https://github.com/rodlukas/UP-admin/commit/GIT_VERSION" className="AppVersion" target="_blank"
-       rel="noopener noreferrer">
+       rel="noopener noreferrer" title="GIT_VERSION">
         GIT_VERSION
     </a>
 
