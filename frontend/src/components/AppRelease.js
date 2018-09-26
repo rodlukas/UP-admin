9ec0e16273f40a4b5cb509c994fec357bd14aebd
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-09-26 11:05:38 +0200

    vlastni komponenta pro nazev release + dodani odkazu na popis release

---
+++ frontend/src/components/AppRelease.js
@@ -0,0 +1,19 @@
+import React, {Fragment} from "react"
+import "./AppVersion.css"
+
+const AppRelease = () => {
+    const version = "GIT_VERSION"
+    return (
+        (version !== "") &&
+            <Fragment>
+                {' ('}
+                <a href={"https://github.com/rodlukas/UP-admin/releases/tag/" + version} target="_blank"
+                   rel="noopener noreferrer" className="AppVersion">
+                    {version}
+                </a>
+                {')'}
+            </Fragment>
+    )
+}
+
+export default AppRelease
