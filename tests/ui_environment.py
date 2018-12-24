d7e85010fb49281d780662d20b0f2a670400387c
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-12-25 00:14:33 +0100

    [testy] oprava nefunkcnich UI testu na CI
    - osetreni prace s oknem v headless modu

--- tests/ui_environment.py
+++ tests/ui_environment.py
@@ -1,16 +1,19 @@
 from selenium import webdriver
 from tests import helpers
+import os
 
 SCREEN_WIDTH = 1920
 
 
 def before_all(context):
     context.browser = webdriver.Firefox()
-    window_width = context.browser.get_window_size()['width']
-    # pozice okna vpravo nahore
-    context.browser.set_window_position(SCREEN_WIDTH - window_width, 0)
-    # automaticka minimalizace okna
-    context.browser.minimize_window()
+    # pokud jsem v GUI rezimu, uprav vlastnosti okna
+    if os.environ.get('MOZ_HEADLESS', 0) == 1:
+        window_width = context.browser.get_window_size()['width']
+        # pozice okna vpravo nahore
+        context.browser.set_window_position(SCREEN_WIDTH - window_width, 0)
+        # automaticka minimalizace okna
+        context.browser.minimize_window()
 
 
 def after_all(context):
