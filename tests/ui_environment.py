05a24830d81b632c752173589a6f17f1bdfad154
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-12-24 23:15:50 +0100

    [testy] automaticka minimalizace okna s prohlizecem pri testovani, pozice vpravo nahore

--- tests/ui_environment.py
+++ tests/ui_environment.py
@@ -1,9 +1,16 @@
 from selenium import webdriver
 from tests import helpers
 
+SCREEN_WIDTH = 1920
+
 
 def before_all(context):
     context.browser = webdriver.Firefox()
+    window_width = context.browser.get_window_size()['width']
+    # pozice okna vpravo nahore
+    context.browser.set_window_position(SCREEN_WIDTH - window_width, 0)
+    # automaticka minimalizace okna
+    context.browser.minimize_window()
 
 
 def after_all(context):
