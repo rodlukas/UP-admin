db293d479db787649f4b1dcedab83bf7c911c4f3
Lukáš Rod <rodlukas@fit.cvut.cz>
2018-12-26 12:45:01 +0100

    [testy] popis magickych konstant

--- tests/ui_environment.py
+++ tests/ui_environment.py
@@ -2,12 +2,15 @@ from selenium import webdriver
 from tests import fixtures
 from selenium.webdriver.firefox.options import Options
 
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+
 
 def before_all(context):
     options = Options()
     options.headless = False
     context.browser = webdriver.Firefox(options=options)
-    context.browser.set_window_size(1920, 1080)
+    context.browser.set_window_size(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 
 def after_all(context):
