b0b3105fe7156fda0bf2c5c010e383d33a046a18
Lukáš Rod <rodlukas@fit.cvut.cz>
2019-02-05 20:00:48 +0100

    [testy] odstraneni zbytecneho (a nefunkcniho) smoke testu djanga

--- admin/tests/test_view.py
+++ admin/tests/test_view.py
@@ -5,7 +5,6 @@ class ViewTest(SimpleTestCase):
     """ Prubeh testu:
         - zkontroluje, zda se podarilo nacist uvodni stranku
         - zkontroluje, zda nactena stranka pouziva spravnou sablonu
-        - zkontroluje, zda nactena stranka obsahuje spravny obsah
     """
     def test_index_page_status_code(self):
         response = self.client.get('/', secure=True)
@@ -15,7 +14,3 @@ class ViewTest(SimpleTestCase):
         response = self.client.get('/', secure=True)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
-
-    def test_index_page_contains_correct_html(self):
-        response = self.client.get('/', secure=True)
-        self.assertContains(response, '<h2>Načítání...</h2>')
