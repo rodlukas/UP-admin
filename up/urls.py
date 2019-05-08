387f176ee325277fbd91b77e26aae7d87f41b0c1
Lukáš Rod <rodlukas@fit.cvut.cz>
2019-05-08 17:13:47 +0200

    [django] pridani robots.txt - zakaz pristupu robotum

--- up/urls.py
+++ up/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('api/v1/', include('api.urls')),
     # favicona pro starsi prohlizece
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('admin/favicon.ico'))),
+    re_path(r'^robots\.txt$', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
     re_path(r'^', TemplateView.as_view(template_name="react-autogenerate.html")),
 ]
 
