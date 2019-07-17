e94b1b6007146d56034bf8aeb23c19ddf145b93f
Lukáš Rod <rodlukas@fit.cvut.cz>
2019-07-17 21:13:14 +0200

    [api] zakladni schema api
    vytvorena kostra schematu api - openapi 3 se swagger-ui, nefunguje "Try it", chybi podrobne popisy

--- up/urls.py
+++ up/urls.py
@@ -3,11 +3,23 @@ from django.views.generic import TemplateView
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 from django.conf import settings
+from rest_framework.schemas import get_schema_view
+from rest_framework import authentication
 
 urlpatterns = [
     path('api/v1/', include('api.urls')),
     # favicona pro starsi prohlizece
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('admin/favicon.ico'))),
+    path('open-api/', get_schema_view(
+        title="ÚPadmin API",
+        description="...",
+        url='/api/v1',  # todo - zatim drf neimplementuje
+        authentication_classes=[authentication.BasicAuthentication]
+    ), name='openapi-schema'),
+    path('docs/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
     re_path(r'^', TemplateView.as_view(template_name="react-autogenerate.html")),
 ]
 
