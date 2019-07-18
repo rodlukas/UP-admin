5652d089bbd58236adce28c352fb3a60b23f3d95
Lukáš Rod <rodlukas@fit.cvut.cz>
2019-07-18 15:02:43 +0200

    [js] oprava title pro edit button
    zobrazil se jen pri najeti na ikonu, ne na cele tlacitko

--- frontend/src/components/buttons/EditButton.js
+++ frontend/src/components/buttons/EditButton.js
@@ -4,8 +4,8 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faPencil} from "@fortawesome/pro-solid-svg-icons"
 
 const EditButton = ({content = "Upravit", onClick, ...props}) =>
-    <Button color="primary" onClick={onClick} {...props}>
-        <FontAwesomeIcon icon={faPencil} title={content}/>
+    <Button color="primary" onClick={onClick} {...props} title={content}>
+        <FontAwesomeIcon icon={faPencil}/>
     </Button>
 
 export default EditButton
