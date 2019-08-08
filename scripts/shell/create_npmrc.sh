61390257d7167de1fca10769a9c4ec27119e592a
Lukáš Rod <rodlukas@fit.cvut.cz>
2019-08-08 13:58:22 +0200

    [ci] lepsi vypisy pri priprave .npmrc

--- scripts/shell/create_npmrc.sh
+++ scripts/shell/create_npmrc.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 if [ -z "${FONTAWESOME_TOKEN}" ]; then
-  echo "FONTAWESOME_TOKEN neni nastaven nebo je prazdny"
+  echo "FONTAWESOME_TOKEN neni nastaven nebo je prazdny, soubor .npmrc neni pripraven"
 else
-  mv frontend/.npmrc.template frontend/.npmrc
-  echo "soubor .nmprc byl uspesne pripraven"
+  if mv frontend/.npmrc.template frontend/.npmrc; then
+    echo "Soubor .npmrc byl uspesne pripraven"
+  else
+    echo "Priprava souboru .npmrc byla neuspesna"
+  fi
 fi
