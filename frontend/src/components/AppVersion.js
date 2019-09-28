88987ea6aa009323ab0b14ea22024296664e21b5
Lukáš Rod <rodlukas@fit.cvut.cz>
2019-09-28 23:08:14 +0200

    [js] oprava barvy odkazu na commit v menu badge

--- frontend/src/components/AppVersion.js
+++ frontend/src/components/AppVersion.js
@@ -1,9 +1,11 @@
 import React from "react"
+import "./AppVersion.css"
 
 const AppVersion = () => (
     <a
         href="https://github.com/rodlukas/UP-admin/commit/%GIT_VERSION"
         target="_blank"
+        className="AppVersion"
         rel="noopener noreferrer"
         title="Zobrazení commitu (GitHub)">
         %GIT_VERSION
