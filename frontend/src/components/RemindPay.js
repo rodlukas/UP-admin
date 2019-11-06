5386874a038f08592c9551d833fc1fc78f8a3ea7
Lukáš Rod <info@lukasrod.cz>
2019-11-06 14:34:50 +0100

    [js] migrace title na tooltips

--- frontend/src/components/RemindPay.js
+++ frontend/src/components/RemindPay.js
@@ -1,16 +1,22 @@
 import { faCommentAltDollar } from "@fortawesome/pro-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import React from "react"
+import React, { Fragment } from "react"
+import { UncontrolledTooltip } from "reactstrap"
 
-const RemindPay = ({ remind_pay }) =>
-    remind_pay && (
-        <FontAwesomeIcon
-            icon={faCommentAltDollar}
-            size="lg"
-            className="text-secondary"
-            transform="up-4"
-            title="Příště platit"
-        />
+const RemindPay = ({ attendance }) =>
+    attendance.remind_pay && (
+        <Fragment>
+            <FontAwesomeIcon
+                icon={faCommentAltDollar}
+                size="lg"
+                className="text-secondary"
+                transform="up-4"
+                id={"RemindPay" + attendance.id}
+            />
+            <UncontrolledTooltip placement="right" target={"RemindPay" + attendance.id}>
+                Příště platit
+            </UncontrolledTooltip>
+        </Fragment>
     )
 
 export default RemindPay
