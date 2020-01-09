3b9f312de011521cfa0e4c994a30a1b202183401
Lukáš Rod <info@lukasrod.cz>
2020-01-09 20:19:50 +0100

    [django] oprava nefunkcni manualni produkce mimo travis

--- up/production_settings.py
+++ up/production_settings.py
@@ -5,9 +5,6 @@ from .settings import *  # lgtm [py/polluting-import]
 
 if os.getenv("TRAVIS"):
     MANUAL_PRODUCTION = True  # pro funkcni testy na Travisu
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "frontend", "dist")
-    ]  # jen na Travisu (zde se pak slozka smaze)
 
 ALLOWED_HOSTS = [
     "uspesnyprvnacek.herokuapp.com",
@@ -29,6 +26,9 @@ SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
 if MANUAL_PRODUCTION:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "frontend", "dist")
+    ]  # jen na Travisu (zde se pak slozka smaze) / manualni produkci
     DEBUG = False
     ALLOWED_HOSTS.append("localhost")
     SECURE_SSL_REDIRECT = False
