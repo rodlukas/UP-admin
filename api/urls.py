a6e2b8f38924851f769bf339fde05797f90f22d6
Lukáš Rod <rodlukas@fit.cvut.cz>
2020-01-16 09:20:44 +0100

    [python] urls.py: dokumentace

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,6 @@
+"""
+Definice mapování URL na jednotlivá view pro API.
+"""
 from django.urls import include, path
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenRefreshSlidingView
@@ -5,6 +8,7 @@ from rest_framework_simplejwt.views import TokenRefreshSlidingView
 from api import views
 from .tokens import MyTokenObtainSlidingView
 
+# DRF router pro namapovani viewsets na mnozinu URL adres
 router = routers.DefaultRouter()
 router.register("clients", views.ClientViewSet)
 router.register(
@@ -18,7 +22,9 @@ router.register("attendancestates", views.AttendanceStateViewSet)
 router.register("applications", views.ApplicationViewSet)
 
 urlpatterns = [
+    # URL z DRF routeru
     path("", include(router.urls)),
+    # dalsi URL mimo DRF
     path("bank/", views.BankView.as_view()),
     path("jwt-auth/", MyTokenObtainSlidingView.as_view(), name="token_obtain"),
     path("jwt-refresh/", TokenRefreshSlidingView.as_view(), name="token_refresh"),
