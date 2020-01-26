5ad41daca5d17ae41d36eae212eae70eacb20efa
Lukáš Rod <rodlukas@fit.cvut.cz>
2020-01-26 16:58:01 +0100

    [python] settings.py && production_settings.py: dokumentace, refaktoring

--- up/production_settings.py
+++ up/production_settings.py
@@ -1,10 +1,16 @@
+"""
+Produkční konfigurace Django projektu.
+Používá se pro nasazené aplikace, případně pro simulaci nasazené aplikace na lokálu (MANUAL_PRODUCTION).
+Rozšiřuje výchozí konfiguraci ze souboru settings.py.
+"""
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 from .settings import *  # lgtm [py/polluting-import]
 
+# pro korektni build a fungovani na Travisu
 if os.getenv("TRAVIS"):
-    MANUAL_PRODUCTION = True  # pro funkcni testy na Travisu
+    MANUAL_PRODUCTION = True
 
 ALLOWED_HOSTS = [
     "uspesnyprvnacek.herokuapp.com",
@@ -19,12 +25,12 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Django konstanty pro bezpecnost
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-
 SECURE_SSL_REDIRECT = True
 SECURE_HSTS_SECONDS = 63072000  # 2 roky
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
+# pravidla pro manualni produkci (pro jeji simulaci na lokalu/build a fungovani Travisu)
 if MANUAL_PRODUCTION:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "frontend", "dist")
