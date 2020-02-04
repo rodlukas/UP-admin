f3025557936ca6057a6747414ba28b03d9dccc8d
Lukáš Rod <rodlukas@fit.cvut.cz>
2020-02-04 20:28:19 +0100

    vylepseni a opravy webpack konfigurace
    - cross-env pro praci s env promennou NODE_ENV, sjednoceni s webpack mode
    - oprava babelu ktery na produkci pracoval v dev modu (takze v bundle byly privatni veci, veci k hot-loader apod., diky tomu byl bundle take zbytecne velky) - opraveno pres cross-env vyse
    - odstraneni zbytecneho html-loaderu
    - sjednoceni a uhlazeni konfigurace loaderu, drobny refaktoring webpack config, doplneni komentaru
    - pripraveni prazdne konfigrace eslint
    - odstraneni nedulezitych objemnych vypisu do konzole

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -10,154 +10,159 @@ const TerserPlugin = require("terser-webpack-plugin")
 // bez .local by nefungovalo na iOS
 const hostName = os.hostname().toLowerCase() + ".local"
 const port = 3000
-const url = `http://${hostName}:${port}/`
+const urlLocal = `http://${hostName}:${port}/`
 const urlProduction = "/static/assets/"
 const htmlFile = "react-autogenerate.html"
 const htmlSource = "src/index.html"
 const htmlTarget = path.resolve(__dirname + "/../", "admin", "templates")
 
-module.exports = (env, argv) => {
-    const isProduction = argv.mode === "production"
+// pouziva se cross-env pro crossplatform nastaveni env promenne
+// napr. kvuli https://github.com/gaearon/react-hot-loader#what-about-production
+const isProduction = process.env.NODE_ENV === "production"
 
-    return {
-        resolve: {
-            alias: {
-                // react-hot-loader
-                "react-dom": "@hot-loader/react-dom"
-            }
-        },
-        entry: ["react-hot-loader/patch", "./src/index"],
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    enforce: "pre",
-                    exclude: /node_modules/,
-                    loader: "eslint-loader",
-                    options: {
-                        cache: true
-                    }
-                },
-                {
-                    test: /\.m?js$/,
-                    exclude: /(node_modules|bower_components)/,
-                    loader: "babel-loader",
-                    options: {
-                        cacheDirectory: true,
-                        cacheCompression: false,
-                        compact: isProduction,
-                        sourceMaps: true,
-                        inputSourceMap: true
-                    }
-                },
-                {
-                    test: /\.html$/i,
-                    loader: "html-loader"
-                },
-                {
-                    test: /\.css$/i,
-                    use: [
-                        isProduction
-                            ? {
-                                  loader: MiniCssExtractPlugin.loader,
-                                  options: {
-                                      hmr: !isProduction,
-                                      sourceMap: true
-                                  }
-                              }
-                            : "style-loader",
-                        {
-                            loader: "css-loader",
-                            options: { importLoaders: 1, sourceMap: true }
-                        },
-                        {
-                            loader: "postcss-loader",
-                            options: {
-                                sourceMap: true,
-                                ident: "postcss",
-                                plugins: [require("postcss-preset-env")(), require("cssnano")()]
-                            }
-                        }
-                    ]
-                }
-            ]
-        },
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: isProduction ? `[name].[contenthash:8].css` : "[name].css",
-                chunkFilename: isProduction
-                    ? `[name].[contenthash:8].chunk.css`
-                    : "[name].chunk.css"
-            }),
-            new CleanWebpackPlugin(),
-            new HtmlWebPackPlugin({
-                // diky teto moznosti muze pak pracovat HtmlWebpackHarddiskPlugin
-                alwaysWriteToDisk: true,
-                template: htmlSource,
-                filename: htmlFile,
-                minify: isProduction
-                    ? {
-                          collapseWhitespace: true,
-                          removeComments: true,
-                          removeRedundantAttributes: true,
-                          removeScriptTypeAttributes: true,
-                          removeStyleLinkTypeAttributes: true,
-                          useShortDoctype: true,
-                          // ignoruj Django sablonovaci jazyk
-                          ignoreCustomFragments: [/{%[\s\S]*?%}/]
-                      }
-                    : false
-            }),
-            // automaticke generovani html souboru do templates slozky s injected odkazy na zdrojove soubory
-            new HtmlWebpackHarddiskPlugin({
-                outputPath: htmlTarget
-            })
-        ],
-
-        output: {
-            chunkFilename: isProduction ? "[name].[contenthash:8].chunk.js" : "[name].chunk.js",
-            filename: isProduction ? "[name].[contenthash:8].js" : "[name].js",
-            path: path.resolve(__dirname, "build/assets"),
-            publicPath: isProduction ? urlProduction : url
-        },
-        devServer: {
-            // pro povoleni pristupu odkudkoliv (a z Djanga)
-            allowedHosts: ["0.0.0.0", hostName],
-            compress: true,
-            headers: { "Access-Control-Allow-Origin": "*" },
-            historyApiFallback: true,
-            host: "0.0.0.0",
-            hot: true,
-            index: htmlFile,
-            overlay: true,
-            port: port
-        },
-        devtool: isProduction ? "source-map" : "cheap-module-source-map",
-        optimization: {
-            minimize: isProduction,
-            splitChunks: {
-                chunks: "all",
-                name: !isProduction
-            },
-            runtimeChunk: {
-                name: entrypoint => `runtime-${entrypoint.name}`
-            },
-            minimizer: [
-                new TerserPlugin({
-                    sourceMap: true
-                }),
-                new OptimizeCSSAssetsPlugin({
-                    cssProcessorOptions: {
-                        map: {
-                            inline: false,
-                            annotation: true
-                        }
-                    },
-                    cssProcessorPluginOptions: {
-                        preset: ["default", { minifyFontValues: { removeQuotes: false } }]
-                    }
-                })
-            ]
+// konfigurace je zalozena:
+// - CRA: https://github.com/facebook/create-react-app/blob/v3.3.1/packages/react-scripts/config/webpack.config.js
+// - castecne se take inspiruje u https://github.com/neutrinojs/neutrino a
+//   https://github.com/insin/nwb/
+// - stavi na puvodni pouzivane konfiguraci https://github.com/rodlukas/UP-admin/blob/0.18.1/frontend/nwb.config.js
+module.exports = {
+    mode: process.env.NODE_ENV,
+    resolve: {
+        alias: {
+            // react-hot-loader
+            "react-dom": "@hot-loader/react-dom"
         }
+    },
+    entry: ["react-hot-loader/patch", "./src/index"],
+    module: {
+        rules: [
+            {
+                test: /\.(js|mjs|jsx|ts|tsx)$/,
+                enforce: "pre",
+                exclude: /node_modules/,
+                loader: "eslint-loader",
+                options: {
+                    cache: true
+                }
+            },
+            {
+                test: /\.(js|mjs)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    cacheDirectory: true,
+                    cacheCompression: false,
+                    compact: isProduction,
+                    sourceMaps: true,
+                    inputSourceMap: true
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    isProduction
+                        ? {
+                              loader: MiniCssExtractPlugin.loader,
+                              options: {
+                                  hmr: !isProduction,
+                                  sourceMap: true
+                              }
+                          }
+                        : "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: { importLoaders: 1, sourceMap: true }
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            sourceMap: true,
+                            ident: "postcss",
+                            plugins: [require("postcss-preset-env")(), require("cssnano")()]
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: isProduction ? `[name].[contenthash:8].css` : "[name].css",
+            chunkFilename: isProduction ? `[name].[contenthash:8].chunk.css` : "[name].chunk.css"
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebPackPlugin({
+            // diky teto moznosti muze pak pracovat HtmlWebpackHarddiskPlugin
+            alwaysWriteToDisk: true,
+            template: htmlSource,
+            filename: htmlFile,
+            minify: isProduction
+                ? {
+                      collapseWhitespace: true,
+                      removeComments: true,
+                      removeRedundantAttributes: true,
+                      removeScriptTypeAttributes: true,
+                      removeStyleLinkTypeAttributes: true,
+                      useShortDoctype: true,
+                      // ignoruj Django sablonovaci jazyk
+                      ignoreCustomFragments: [/{%[\s\S]*?%}/]
+                  }
+                : false
+        }),
+        // automaticke generovani html souboru do templates slozky s injected odkazy na zdrojove soubory
+        new HtmlWebpackHarddiskPlugin({
+            outputPath: htmlTarget
+        })
+    ],
+
+    output: {
+        // nazvy souboru odpovidaji https://create-react-app.dev/docs/production-build/
+        chunkFilename: isProduction ? "[name].[contenthash:8].chunk.js" : "[name].chunk.js",
+        filename: isProduction ? "[name].[contenthash:8].js" : "[name].js",
+        path: path.resolve(__dirname, "build/assets"),
+        publicPath: isProduction ? urlProduction : urlLocal
+    },
+    devServer: {
+        // pro povoleni pristupu odkudkoliv (a z Djanga)
+        allowedHosts: ["0.0.0.0", hostName],
+        compress: true,
+        headers: { "Access-Control-Allow-Origin": "*" },
+        historyApiFallback: true,
+        host: "0.0.0.0",
+        hot: true,
+        index: htmlFile,
+        overlay: true,
+        port: port,
+        stats: {
+            modules: false
+        }
+    },
+    stats: { children: false, modules: false },
+    devtool: isProduction ? "source-map" : "cheap-module-source-map",
+    optimization: {
+        minimize: isProduction,
+        splitChunks: {
+            chunks: "all",
+            name: !isProduction
+        },
+        runtimeChunk: {
+            name: entrypoint => `runtime-${entrypoint.name}`
+        },
+        minimizer: [
+            new TerserPlugin({
+                sourceMap: true
+            }),
+            new OptimizeCSSAssetsPlugin({
+                cssProcessorOptions: {
+                    map: {
+                        inline: false,
+                        annotation: true
+                    }
+                },
+                cssProcessorPluginOptions: {
+                    preset: ["default", { minifyFontValues: { removeQuotes: false } }]
+                }
+            })
+        ]
     }
 }
