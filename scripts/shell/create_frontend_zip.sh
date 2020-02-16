48cfd12506f8817c0bf4fc4cfc9c1d8312db1ca3
Lukáš Rod <rodlukas@fit.cvut.cz>
2020-02-16 22:40:49 +0100

    [ci] vylepseny vypis pri tvorbe frontend.zip

--- scripts/shell/create_frontend_zip.sh
+++ scripts/shell/create_frontend_zip.sh
@@ -4,4 +4,6 @@
 
 if [ -n "$TRAVIS_TAG" ]; then
   zip --must-match --recurse-paths frontend.zip frontend/build admin/templates/react-autogenerate.html --exclude frontend/build/assets/react-autogenerate.html
+else
+  echo "frontend.zip se nevytvoril, protoze se nejedna o tagged commit"
 fi
