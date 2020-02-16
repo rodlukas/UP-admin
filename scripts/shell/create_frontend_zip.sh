8cf2e36d34476eeae59503c4e38d351a45fd8200
Lukáš Rod <rodlukas@fit.cvut.cz>
2020-02-16 22:07:19 +0100

    [ci] vylepsena prace s frontend.zip
    pro tagged commity se zip vytvori a pak se smaze po GH deploy, v opacnem pripade se nevytvori ani nemaze

---
+++ scripts/shell/create_frontend_zip.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# pokud se jedna o tagged commit, vytvor frontend.zip
+# se sestavenym frontendem (vcetne template) pro nahrani na GH
+
+if [ -n "$TRAVIS_TAG" ]; then
+  zip --must-match --recurse-paths frontend.zip frontend/build admin/templates/react-autogenerate.html --exclude frontend/build/assets/react-autogenerate.html
+fi
