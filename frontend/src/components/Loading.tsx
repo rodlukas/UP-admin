763663f665cfac6fc91881df2dddcbcc90bb014c
Lukáš Rod <rodlukas@fit.cvut.cz>
2020-03-03 19:31:14 +0100

    [js] Loading: upozorneni na presprilis dlouhe nacitani

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -1,8 +1,16 @@
 import { faSpinnerThird } from "@fortawesome/pro-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import * as React from "react"
+import { Alert } from "reactstrap"
 
 const LONG_LOADING_THRESHOLD = 5 // sekundy
+const OVERLONG_LOADING_THRESHOLD = 25 // sekundy
+
+const LOADING_STATE = Object.freeze({
+    NORMAL_LOADING: 0,
+    LONG_LOADING: 1,
+    OVERLONG_LOADING: 2
+})
 
 type Props = {
     text?: string
@@ -10,14 +18,24 @@ type Props = {
 
 /** Komponenta zobrazující načítání v aplikaci. */
 const Loading: React.FunctionComponent<Props> = ({ text = "Načítání" }) => {
-    const [longLoading, setLongLoading] = React.useState(false)
+    const [loadingState, setLoadingState] = React.useState(LOADING_STATE.NORMAL_LOADING)
+
+    function setLoadingTimeout(loadingState: number): number {
+        return window.setTimeout(
+            () => setLoadingState(loadingState),
+            (loadingState === LOADING_STATE.LONG_LOADING
+                ? LONG_LOADING_THRESHOLD
+                : OVERLONG_LOADING_THRESHOLD) * 1000
+        )
+    }
 
     React.useEffect(() => {
-        const timeoutId = window.setTimeout(
-            () => setLongLoading(true),
-            LONG_LOADING_THRESHOLD * 1000
-        )
+        const timeoutId = setLoadingTimeout(LOADING_STATE.LONG_LOADING)
+        return (): void => window.clearTimeout(timeoutId)
+    }, [])
 
+    React.useEffect(() => {
+        const timeoutId = setLoadingTimeout(LOADING_STATE.OVERLONG_LOADING)
         return (): void => window.clearTimeout(timeoutId)
     }, [])
 
@@ -26,7 +44,12 @@ const Loading: React.FunctionComponent<Props> = ({ text = "Načítání" }) => {
             <FontAwesomeIcon icon={faSpinnerThird} spin size="3x" />
             <br />
             {text}...
-            {longLoading && " Stále pracuji 😎"}
+            {loadingState === LOADING_STATE.LONG_LOADING && " Stále pracuji 😎"}
+            {loadingState === LOADING_STATE.OVERLONG_LOADING && (
+                <Alert color="warning" className="mt-1">
+                    ⚠ Načítání trvá příliš dlouho, mohlo dojít k chybě. Zkuste stránku načíst znovu.
+                </Alert>
+            )}
         </div>
     )
 }
