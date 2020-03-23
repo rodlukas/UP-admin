3ff69b8ac5afe4d329cb4c67bac95e63b7f15ec9
Lukáš Rod <rodlukas@fit.cvut.cz>
2020-03-23 20:17:02 +0100

    [js] gdpr.js: camelcase nazev promenne
    sonarcloud

--- admin/static/admin/gdpr.js
+++ admin/static/admin/gdpr.js
@@ -1,12 +1,12 @@
 // funkce pro prepnuti modu skryti soukromych informaci klientu
 function toggleGdpr() {
-    const gdpr_attr = "gdpr"
+    const gdprAttr = "gdpr"
     // zapni/vypni soukromy mod (pokud ma body prislusnou tridu, vypni mod - odstran ji, jinak zapni - pridej ji)
-    if (document.body.classList.contains(gdpr_attr)) {
-        document.body.classList.remove(gdpr_attr)
+    if (document.body.classList.contains(gdprAttr)) {
+        document.body.classList.remove(gdprAttr)
         return "soukromý režim VYPNUTÝ"
     } else {
-        document.body.classList.add(gdpr_attr)
+        document.body.classList.add(gdprAttr)
         return "soukromý režim ZAPNUTÝ"
     }
 }
