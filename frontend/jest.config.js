99f405f65418b9e56f81f7cc473d14278dee38cb
Lukáš Rod <rodlukas@fit.cvut.cz>
2020-09-05 21:53:56 +0200

    [tests] jest - funkcni jazyky pro node

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,3 +1,6 @@
+// pro kompletni podporu jazyku v node (napr. date.toLocaleString)
+process.env.NODE_ICU_DATA = "node_modules/full-icu"
+
 module.exports = {
     setupFilesAfterEnv: ["./jest.setup.ts"],
 }
