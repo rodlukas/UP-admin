9039274a57ad92efb392fc6beb011404b7a8089f
Lukáš Rod <rodlukas@fit.cvut.cz>
2020-09-05 23:24:32 +0200

    [ci] geckodriver update + refaktoring

--- scripts/shell/install_geckodriver.sh
+++ scripts/shell/install_geckodriver.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+VERSION=0.27.0
+GITHUB_REPO_URL=https://github.com/mozilla/geckodriver
+
 mkdir geckodriver
 # stazeni a rozbaleni geckodriveru
-wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz -P geckodriver
-tar -xzf geckodriver/geckodriver-v0.26.0-linux64.tar.gz -C geckodriver
+wget ${GITHUB_REPO_URL}/releases/download/v${VERSION}/geckodriver-v${VERSION}-linux64.tar.gz -P geckodriver
+tar -xzf geckodriver/geckodriver-v${VERSION}-linux64.tar.gz -C geckodriver
 # nastaveni do PATH
 export PATH=$PATH:$PWD/geckodriver
