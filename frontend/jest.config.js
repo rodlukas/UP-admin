e70e34c2c771b79d6e289b87a2975c678e6e5fd9
Lukáš Rod <rodlukas@fit.cvut.cz>
2020-09-05 23:13:55 +0200

    [tests] node full-icu fix - node 14 upgrade

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,3 @@
-// pro kompletni podporu jazyku v node (napr. date.toLocaleString)
-process.env.NODE_ICU_DATA = "node_modules/full-icu"
-
 module.exports = {
     setupFilesAfterEnv: ["./jest.setup.ts"],
 }
--- scripts/shell/travis/js_install.sh
+++ scripts/shell/travis/js_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # travis zatim jinak nepodporuje nodejs 12
-nvm install 12
+nvm install 14
 npm i -g npm@6
 npm install -g yarn@1
 yarn install
