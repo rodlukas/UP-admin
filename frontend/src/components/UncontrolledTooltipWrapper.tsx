4b9b9df60d234cbefc000ae6caa68085a89d9cfc
Lukáš Rod <rodlukas@fit.cvut.cz>
2020-11-21 19:12:16 +0100

    [js] {children} cleanup

--- frontend/src/components/UncontrolledTooltipWrapper.tsx
+++ frontend/src/components/UncontrolledTooltipWrapper.tsx
@@ -4,12 +4,7 @@ import { UncontrolledTooltip, UncontrolledTooltipProps } from "reactstrap"
 /** Wrapper pro UncontrolledTooltip zajišťující vhodné výchozí hodnoty. */
 const UncontrolledTooltipWrapper: React.FC<UncontrolledTooltipProps> = ({
     placement = "auto",
-    children,
     ...props
-}) => (
-    <UncontrolledTooltip placement={placement} {...props}>
-        {children}
-    </UncontrolledTooltip>
-)
+}) => <UncontrolledTooltip placement={placement} {...props} />
 
 export default UncontrolledTooltipWrapper
