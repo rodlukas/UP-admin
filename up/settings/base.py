be2197815270485c80262f02f594102adeef9a0f
Lukáš Rod <rodlukas@fit.cvut.cz>
2020-12-23 22:45:29 +0100

    [python] bump & cleanup deps

--- up/settings/base.py
+++ up/settings/base.py
@@ -150,21 +150,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
 # debug toolbar
-DEBUG_TOOLBAR_PANELS = [
-    "ddt_request_history.panels.request_history.RequestHistoryPanel",
-    "debug_toolbar.panels.versions.VersionsPanel",
-    "debug_toolbar.panels.timer.TimerPanel",
-    "debug_toolbar.panels.settings.SettingsPanel",
-    "debug_toolbar.panels.headers.HeadersPanel",
-    "debug_toolbar.panels.request.RequestPanel",
-    "debug_toolbar.panels.sql.SQLPanel",
-    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
-    "debug_toolbar.panels.templates.TemplatesPanel",
-    "debug_toolbar.panels.cache.CachePanel",
-    "debug_toolbar.panels.signals.SignalsPanel",
-    "debug_toolbar.panels.logging.LoggingPanel",
-    "debug_toolbar.panels.redirects.RedirectsPanel",
-]
 DEBUG_TOOLBAR_CONFIG = {
     "SHOW_TOOLBAR_CALLBACK": lambda request: True if DEBUG else False,
     "SHOW_COLLAPSED": True,
