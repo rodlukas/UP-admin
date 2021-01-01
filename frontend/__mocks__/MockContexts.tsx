4e878d53f1b4644d41d00ef495d4d50a1d6813f6
Lukáš Rod <rodlukas@fit.cvut.cz>
2021-01-01 14:12:31 +0100

    [js] bump deps & fix lint

--- frontend/__mocks__/MockContexts.tsx
+++ frontend/__mocks__/MockContexts.tsx
@@ -1,8 +1,10 @@
 import * as React from "react"
+
 import { AttendanceStatesContext } from "../src/contexts/AttendanceStatesContext"
 import { ClientsActiveContext } from "../src/contexts/ClientsActiveContext"
 import { GroupsActiveContext } from "../src/contexts/GroupsActiveContext"
 import { noop } from "../src/global/utils"
+
 import * as data from "./data.json"
 
 const MockContexts: React.FC = (props) => (
