098859ccc0dda8941e872d82e3c067e2954a48a7
Lukáš Rod <lukasekrod@gmail.com>
2022-11-15 21:07:27 +0000

    wip

--- scripts/shell/release_tasks.sh
+++ scripts/shell/release_tasks.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export DJANGO_SETTINGS_MODULE=up.settings.production
-python manage.py collectstatic --noinput
-python manage.py migrate
+pipenv run python manage.py collectstatic --noinput
+pipenv run python manage.py migrate
