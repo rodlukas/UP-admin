30cd9cf29b916ab61d85ed186e44a80055abe89e
Lukáš Rod <rodlukas@fit.cvut.cz>
2022-11-17 13:26:37 +0100

    debug

--- tests/ui_steps/login_logout.py
+++ tests/ui_steps/login_logout.py
@@ -49,7 +49,7 @@ def login(context, username, password):
     context.browser.find_element_by_tag_name('body')
     context.browser.save_screenshot('screenshot.png')
     #print(requests.get('http://localhost:8000/static/assets/runtime-main.72fd7f6a.js'))
-    DrawImage.from_file("screenshot.png").draw_image()
+    #DrawImage.from_file("screenshot.png").draw_image()
     DrawImage.from_url("https://www.google.com/logos/doodles/2022/czech-republic-freedom-and-democracy-day-2022-6753651837109659-s.png").draw_image()
     # pockej az bude dostupny prihlasovaci formular
     wait_form_login_visible(context.browser)
