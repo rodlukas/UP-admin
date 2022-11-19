d892d2de1b03b9898f3d04a6b4447bcc1f0ac0ca
Lukáš Rod <rodlukas@fit.cvut.cz>
2022-11-19 20:58:44 +0100

    wip

--- scripts/shell/ci/create_npmrc.sh
+++ scripts/shell/ci/create_npmrc.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-if [ -z "${GPR_TOKEN}" ]; then
-  echo "❌ CHYBA: Soubor .npmrc neni pripraven. GPR_TOKEN (Github Package Registry token pro PRO verzi FontAwesome) neni nastaven nebo je prazdny."
+if mv frontend/.npmrc.template frontend/.npmrc; then
+  echo "✅ Soubor .npmrc je uspesne pripraven."
 else
-  if mv frontend/.npmrc.template frontend/.npmrc; then
-    echo "✅ Soubor .npmrc je uspesne pripraven."
-  else
-    echo "❌ CHYBA: Priprava souboru .npmrc selhala."
-  fi
+  echo "❌ CHYBA: Priprava souboru .npmrc selhala."
 fi
