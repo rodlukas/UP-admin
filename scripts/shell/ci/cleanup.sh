f98fb0c6f672724bad21955fdec40e4058e84e83
Lukáš Rod <lukasekrod@gmail.com>
2022-11-20 07:21:32 +0000

    add deploy workflow for ghactions

---
+++ scripts/shell/ci/cleanup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rm -rf geckodriver geckodriver.log coverage.xml docs frontend tests node_modules .idea codecov .mypy_cache .behaverc .coveragerc .editorconfig .env.template .env mypy.ini .prettierignore .prettierrc.json pyproject.toml .codecov.yml
