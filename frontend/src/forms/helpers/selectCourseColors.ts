86df8b27ae50ce0fa899a5a9451bbdd06d845931
Lukáš Rod <info@lukasrod.cz>
2025-12-09 21:54:01 +0100

    fix dot

--- frontend/src/forms/helpers/selectCourseColors.ts
+++ frontend/src/forms/helpers/selectCourseColors.ts
@@ -3,6 +3,8 @@ import { CSSObjectWithLabel, StylesConfig } from "react-select"
 
 import { CourseType } from "../../types/models"
 
+const PLACEHOLDER_DOT_COLOR = "#ccc"
+
 // kolecko zobrazene v react-select
 const dot = (color = "transparent"): CSSObjectWithLabel => ({
     alignItems: "center",
@@ -16,6 +18,8 @@ const dot = (color = "transparent"): CSSObjectWithLabel => ({
         marginRight: 8,
         height: 14,
         width: 14,
+        // v input komponente dochazi ke shrinku a z dot fakt fazoli nechceme!
+        flexShrink: 0,
     },
 })
 
@@ -47,8 +51,15 @@ export const selectStyles: StylesConfig<CourseType> = {
             },
         }
     },
-    input: (styles) => ({ ...styles, ...dot() }),
-    placeholder: (styles) => ({ ...styles, ...dot("#ccc") }),
+    // Ta podminka vypada trochu divne, ale je to proto, ze value mame jen tehdy, kdyz je neco rozepsane v inputu,
+    // po vyberu option uz value zase nemame, my tedy pri psani chceme videt placeholder barvu a kdykoliv jinak bud barvu optiony,
+    // nebo barvu z placeholderu nize.
+    // React-select v3 zde fungoval trochu jinak a podminka tu nebyla potreba, kdybychom tu ale placli barvu natvrdo, tak nam prekryje barvu optiony.
+    input: (styles, { value }) => ({
+        ...styles,
+        ...dot(value ? PLACEHOLDER_DOT_COLOR : "transparent"),
+    }),
+    placeholder: (styles) => ({ ...styles, ...dot(PLACEHOLDER_DOT_COLOR) }),
     singleValue: (styles, { data }) => ({
         ...styles,
         ...dot(data.color),
