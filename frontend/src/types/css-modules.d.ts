2dba48509928de25c0bf7406c3eed058f5c34bfc
Lukáš Rod <lukas.rod@ceskatelevize.cz>
2025-12-23 22:14:28 +0100

    comment fix

--- frontend/src/types/css-modules.d.ts
+++ frontend/src/types/css-modules.d.ts
@@ -1,7 +1,3 @@
-// TypeScript deklarace pro CSS Modules
-// Tento soubor umožní TypeScriptu rozpoznat importy CSS Modules
-// a poskytne autocomplete a type checking pro CSS třídy
-
 declare module "*.module.css" {
     const classes: Readonly<Record<string, string>>
     export default classes
