c5cab60a455b3ce6393cdcb90fbf26622c176a7e
rodlukas <lukasekrod@gmail.com>
2026-09-13 19:08:01 +0200

    wip

--- scripts/shell/release_tasks.sh
+++ scripts/shell/release_tasks.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+# bez set -e by selhany collectstatic zustal skryty: skript by dobehl dal, vratil nulu
+# a chyba by se projevila az jinde jako chybejici zaznam v manifestu statiky
+set -e
+
 # DEPLOYED drzi debug_toolbar mimo INSTALLED_APPS, aby se nesbirala jeho statika (testy
 # ji nepotrebuji - pri `manage.py test`/`behave` je TESTS_RUNNING true, takze toolbar
-# v INSTALLED_APPS stejne neni); --ignore vynecha source mapy, ktere patri do Sentry
-DEPLOYED=True pipenv run python manage.py collectstatic --noinput --ignore="*.map"
+# v INSTALLED_APPS stejne neni)
+DEPLOYED=True pipenv run python manage.py collectstatic --noinput
 pipenv run python manage.py migrate
 pipenv run python manage.py createcachetable
